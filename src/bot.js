a7c21fc5bd7946313a6da0658e154bf0c5c14133
GaryTopHat <garywallace@hotmailt.fr>
2017-12-10 04:08:26 -0500

    Fix issue

--- src/bot.js
+++ src/bot.js
@@ -103,7 +103,7 @@ function welcome(session) {
 function displayAllBots(session) {
   _bot.dbStore.fetch("SELECT username FROM registered_bots").then((bots) => {
     // :bulb:
-    let msg = bots ? "Here is the list of all registered bots:\n" + prettyPrintList(bots) : "The list is empty.";
+    let msg = bots ? ("Here is the list of all registered bots:\n" + prettyPrintList(bots)) : "The list is empty.";
 
     sendMessage(session, msg);
   }).catch((err) => {
