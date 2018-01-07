27e3f541a5226a25c826145cbfeecbc95f04803e
GaryTopHat <garywallace@hotmailt.fr>
2018-01-06 21:04:17 -0500

    Update ND schema

--- src/bot.js
+++ src/bot.js
@@ -53,15 +53,19 @@ CREATE TABLE IF NOT EXISTS registered_bots (
 	entry_modified_by VARCHAR,
     is_online BOOLEAN DEFAULT TRUE,
   is_working_properly BOOLEAN DEFAULT TRUE,
-  reputation_score integer NULL,
-  average_rating integer NULL,
-  review_count integer NULL
+  reputation_score decimal NULL,
+  average_rating decimal NULL,
+  review_count decimal NULL
 );
-ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS reputation_score integer NULL;
-ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS average_rating integer NULL;
-ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS review_count integer NULL;
-`;
+ALTER TABLE registered_bots DROP COLUMN IF EXISTS reputation_score;
+ALTER TABLE registered_bots DROP COLUMN IF EXISTS average_rating;
+ALTER TABLE registered_bots DROP COLUMN IF EXISTS review_count;
 
+ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS reputation_score decimal NULL;
+ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS average_rating decimal NULL;
+ALTER TABLE registered_bots ADD COLUMN IF NOT EXISTS review_count decimal NULL;
+`;
+//TODO remove the drop columns
 _bot.onReady = () => {
   _bot.dbStore = new PsqlStore(_bot.client.config.storage.postgres.url, process.env.STAGE || 'development');
   _bot.dbStore.initialize(DATABASE_TABLES).then(() => {}).catch((err) => {
