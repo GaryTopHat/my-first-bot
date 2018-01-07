4e748712571abf9cdf2539f54961e0d5606f9a0f
GaryTopHat <garywallace@hotmailt.fr>
2018-01-07 12:41:09 -0500

    fix issue

--- src/bot.js
+++ src/bot.js
@@ -266,7 +266,7 @@ function insertNewBot(session, newBot)
 
 function updateBot(session, bot)
 {
-  _bot.dbStore.execute("UPDATE registered_bots SET (entry_modified_by='$1', entry_modified_on=$2, reputation_score=$3, average_rating=$4, review_count=$5) WHERE toshi_id='$6'", 
+  _bot.dbStore.execute("UPDATE registered_bots SET entry_modified_by='$1', entry_modified_on=$2, reputation_score=$3, average_rating=$4, review_count=$5 WHERE toshi_id='$6'", 
   [session.user.toshi_id, Date.now, bot.reputation_score, bot.average_rating, bot.review_count, bot.toshi_id,])
   .then(() => {
 
