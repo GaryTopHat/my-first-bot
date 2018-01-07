ef0edbbb2438d4374caa56e672e68ad050887526
GaryTopHat <garywallace@hotmailt.fr>
2018-01-07 15:40:21 -0500

    Fix issue

--- src/bot.js
+++ src/bot.js
@@ -129,7 +129,7 @@ function updateResgisteredBotsData(session){
 
       if(botsFound){ 
         //Delete bot where the toshi_id does not point to the same username anymore. Owner need to add it again.
-        botsFound.results.filter(bot => usernames.indexOf(bot.username) > -1).map(bot => deleteBotByUsername(session, bot.username));
+        botsFound.results.filter(bot => usernames.indexOf(bot.username) = -1).map(bot => deleteBotByUsername(session, bot.username));
         botsFound.results.filter(bot => toshi_ids.indexOf(bot.toshi_id) > -1).map(bot => updateBot(session, bot));
           
       }
@@ -289,7 +289,7 @@ function deleteBotByUsername(session, username)
   [username])
   .then(() => {
 
-    Logger.info("@" + username + " was removed from the list for toshi_id/username inconsistency.")
+    Logger.info("@" + username + " was removed from the list.")
   }).catch((err) => {
     Logger.error(err);
   });
